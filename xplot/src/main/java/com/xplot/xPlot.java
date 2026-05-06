6046e02ca511821af069e87b6547975f9e6f640b
Tommy Xu <tommyxu@Tommys-MacBook-Pro.local>
2026-05-05 22:22:59 -0400

    made test panel to check if maven works

--- xplot/src/main/java/com/xplot/xPlot.java
+++ xplot/src/main/java/com/xplot/xPlot.java
@@ -1,15 +1,20 @@
 package com.xplot;
 import javafx.stage.*;
 import javafx.scene.*;
+import javafx.scene.control.Label;
 import javafx.event.*;
 import javafx.application.Application;
 
-public class XPlot extends Application {
+public class xPlot extends Application {
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage mainStage) throws Exception {
+        Label label = new Label("oleleolala");
         
+        mainStage.setTitle("xPlot");
+        mainStage.setScene(new Scene(label, 800, 600));
+        mainStage.show();
     }
 }
